35eb6a62f6ab64444446383b179b49ac372a2ec9
Ilya Fakhrutdinov <bekamez@gmail.com>
2022-12-11 16:29:14 +0500

    build(db): add database url to db client config

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -3,11 +3,12 @@ import { Options } from '@mikro-orm/core';
 import { BaseEntity, User, Board, BoardDetails, List, Card } from './entities';
 
 const IS_DEV = process.env.NODE_ENV === 'development';
-const { DB_NAME, DB_USER, DB_PASSWORD } = process.env;
+const { DATABASE_URL, DB_NAME, DB_USER, DB_PASSWORD } = process.env;
 
 const options: Options = {
   type: 'mongo',
   entities: [User, Board, BoardDetails, List, Card, BaseEntity],
+  clientUrl: DATABASE_URL,
   dbName: DB_NAME,
   user: DB_USER,
   password: DB_PASSWORD,
